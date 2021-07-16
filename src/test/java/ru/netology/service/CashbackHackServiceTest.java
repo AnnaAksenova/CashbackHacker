1a45084ca9f466ae84e8e8194cbe2879d92c20c2
Anna Aksenova <annaa_aksenova@mail.ru>
2021-07-16 21:58:51 +0300

    Testing files

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturn1IfAmountIs999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+
+        int expected = 1;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReturn400IfAmountIs600() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 600;
+
+        int actual = service.remain(amount);
+
+        int expected = 400;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+
+        int expected = 0;
+
+        assertEquals(expected,actual);
+    }
+}

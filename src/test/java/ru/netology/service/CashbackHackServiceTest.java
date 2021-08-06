9eac7c592000c3dc5f1c03bacded3ec2d771969d
Anna Aksenova <annaa_aksenova@mail.ru>
2021-08-06 18:32:38 +0300

    fall test on junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -29,5 +29,50 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected,actual);
     }
+    @Test
+    public void shouldReturn1IfAmountIs999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
 
+        int actual = service.remain(amount);
+
+        int expected = 1;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReturn400IfAmountIs600() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 600;
+
+        int actual = service.remain(amount);
+
+        int expected = 400;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+
+        int expected = 0;
+
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturn0IfAmountEqual1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+
+        int expected = 0;
+
+        assertEquals(expected,actual);
+    }
 }
